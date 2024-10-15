4578272eff3f08c569abc9dff76fe1fa1d3b3b0c
Jkeroro <104914788+Jkeroromk@users.noreply.github.com>
2024-10-15 12:39:14 -0400

    Add files via upload

--- key.js
+++ key.js
@@ -1,11 +1,8 @@
-let songs;
 // f5eb93d5379d4b3b9dcbd6ea3d8d4a31 client secret
 // a46a3ac979964d9d8f3cb3152e3a1cbe client id 
 
 const clientId = 'a46a3ac979964d9d8f3cb3152e3a1cbe';
 const clientSecret = '961849266f2f41bd94c05f31fc67c01c';
-const encodedAuth = btoa(`${clientId}:${clientSecret}`);
-
 
 async function getAccessToken() {
     const authString = `${clientId}:${clientSecret}`;
@@ -19,12 +16,6 @@ async function getAccessToken() {
         },
         body: 'grant_type=client_credentials'
     });
-
-    if (!response.ok) {
-        const errorData = await response.json();
-        throw new Error(`Error fetching access token: ${errorData.error}`);
-    }
-
     const data = await response.json();
     return data.access_token;
 }
